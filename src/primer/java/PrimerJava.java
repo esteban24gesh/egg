9e4c460974b91d98750b9bc6f0d8bb1cf360e085
esteban <esteban24gesh@gmail.com>
2022-05-18 12:09:40 -0300

    primer commit

---
+++ src/primer/java/PrimerJava.java
@@ -0,0 +1,18 @@
+
+package primer.java;
+
+import java.util.Scanner;
+
+
+public class PrimerJava {
+
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        String nombre;
+        System.out.println("Ingrese su nombre");
+        nombre = leer.next();
+        System.out.println("hola mundo! soy " + nombre + " y tengo hambre");
+    }
+    
+}
